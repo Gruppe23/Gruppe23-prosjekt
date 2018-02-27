af9b690b27343ac4baedc5fc85d3483eaff5f199
AndreasFVangen <35685681+AndreasFVangen@users.noreply.github.com>
2018-02-27 14:08:22 +0100

    Mer react arbeid
    Laget registreringsvindu, men er uoptimalt, skal se om jeg forstår React State for å oppdatere react istedet for å overwrite. Mekker ny branch.

--- src/js/app.js
+++ src/js/app.js
@@ -6,33 +6,81 @@ var passwordHash = require('password-hash');
 var hashedPassword = passwordHash.generate('bass32');
 console.log(hashedPassword)
 
-class Start extends React.Component {
+//============================================================================================================================
+//=========================================Login / Regster - RENDER========================================================
+//============================================================================================================================
+class LoginRender extends React.Component {
   render() {
     return (
     <div className="login">
     <span className="title">Røde Kors</span>
-      <div className="loginContent">
-        <div className="loginelements">
-        <span>login<p></p></span>
-        <input type="text" id="loginMail"></input><p></p>
-        <span>password<p></p></span>
-        <input type="password" id="loginPassword"></input><p></p>
-        <button onClick={login}>Login</button>
-        <p></p>
-        <button onClick={forside}>Load Frontpage</button>
-        </div>
+      <div className="loginContent" id="loginContent">
+{/*  Vi har innholdet innad i firkanten vi ser på login i en egen render, fordi vi skal ha registreringsvinduet
+  i samme firkanten. Slik at vi kan rendre login tilbake og ikke rendre like mye.*/}
+          <LoginWindow />
       </div>
     </div>
     );
   }
 }
-function start(){
+
+//Vi lager elementene som vi bruker til å logge inn med. Samt en register knapp som lar oss bytte innholdet i loginelements div'en til et registreringsskjema.
+class LoginWindow extends React.Component {
+  render() {
+    return (
+      <div className="loginelements">
+        <span>Login<p></p></span>
+        <input type="text" id="loginMail"></input><p></p>
+        <span>Password<p></p></span>
+        <input type="password" id="loginPassword"></input><p></p>
+        <button onClick={login}>Login</button>
+        <p></p>
+        <button onClick={forside}>Load Frontpage</button>
+        <p></p>
+        <button onClick={renderRegister}>Til Registrering</button>
+      </div>
+    );
+  }
+}
+
+class Register extends React.Component {
+  render() {
+    return (
+      <div className="registerelements">
+        <span>Fornavn: </span> <input type="text" id="registerFirstName"></input><p></p>
+        <span>Etternavn: </span> <input type="text" id="registerFirstName"></input><p></p>
+        <span>Mail: </span> <input type="text" id="registerMail"></input><p></p>
+        <span>Passord: </span> <input type="password" id="registerPassword"></input><p></p>
+        <span>Passord Controll: </span> <input type="password" id="registerControllPassword"></input><p></p>
+        <button>Registrer</button>
+        <p></p>
+        <button>Tilbake til login</button>
+        <p></p>
+        <button onClick={forside}>Load Frontpage</button>
+        <p></p>
+      </div>
+    );
+  }
+}
+
+//På appstart får vi opp login vinduet. Altså klassen LoginRender blir loadet. Klassen LoginRender
+//rendrer automatisk loginvinduet. og har muligheten til å rendre registreringsvinduet innad i seg selv
+//Fra en knapp.
+
+function renderLogin(){
   ReactDOM.render((
-    <Start />
+    <LoginRender />
   ), document.getElementById("all"));
 }
 
-start()
+function renderRegister(){
+  ReactDOM.render((
+    <Register />
+  ), document.getElementById("loginContent"))
+}
+
+renderLogin()
+
 
 
 
@@ -52,23 +100,26 @@ class Forside extends React.Component {
         <button ref="kalenderbutton">
         kalender
         </button>
-        <button ref="Brukeroversikt">
+        <button ref="brukeroversikt">
           Brukeroversikt
         </button>
         </div>
         <div id="root">
         <span className="overskrift">Velkommen til Forsiden!</span>
-        <button onClick={start}> Gå tilbake til testside </button>
+        <button onClick={renderLogin}> Gå tilbake til testside </button>
         </div>
       </div>
     );
   }
     componentDidMount() {
       this.refs.forsidebutton.onclick = () => {
-        start()
+        renderLogin()
       };
     }
 }
+//======================================================================================================
+//======================================================================================================
+//======================================================================================================
 function login(){
   let mail = document.getElementById("loginMail").value
   let pass = document.getElementById("loginPassword").value
