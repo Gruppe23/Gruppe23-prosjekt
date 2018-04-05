8d15dab916a590b84b41e65ef7f46a97992b67ad
Tsikio <Tsikio@noreply.github.com>
2018-04-05 13:20:55 +0200

    Testing av funksjonalitet react-calendar
    Har prøvd å ta ibruk propsene til react-calendar modulen, slet en del med hvor man skal kjøre props-funksjonene. Klarte ikke implementere css-styling, får en error som sier unexpected token. Bytt ut importen med 'react-calendar' for å se feilen.

--- src/js/ForsidePaths/kalender.js
+++ src/js/ForsidePaths/kalender.js
@@ -3,13 +3,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Link, HashRouter, Switch, Route } from 'react-router-dom';
+import Calendar from 'react-calendar/dist/entry.nostyle';
 
 
 class Kalender extends React.Component<{}> {
   render() {
     return(
-      <div className="full kalender"> Kalender </div>
+      <div className="full kalender"> Kalender
+        <Calendar
+          className="react-calendar"
+          showWeekNumber = {true}
+          onClickDay = {(value) => alert('Clicked day: ', value)}
+         />
+
+      </div>
     )
+  }
+  componentDidMount(){
+
   }
 }
 
