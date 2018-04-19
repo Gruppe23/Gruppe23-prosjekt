5f9d7d69536961abe6d129430bcdb78c9aaeea77
Tsikio <Tsikio@noreply.github.com>
2018-04-19 12:40:02 +0200

    something
    slkdjg

--- src/js/ForsidePaths/welcomepage.js
+++ src/js/ForsidePaths/welcomepage.js
@@ -4,13 +4,76 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Link, HashRouter, Switch, Route } from 'react-router-dom';
 
+const divStyle = {
+  position: "relative"
+};
 
+const topContainer = {
+  height: "75%"
+};
+
+const kalenderShow = {
+  bottom: "0",
+  position: "absolute",
+  height: "150px",
+  width: "100%"
+};
+
+const topLeftContainer = {
+  float: "left"
+}
+
+const topLeftInner1 = {
+
+}
+
+const topleftInner2 = {
+
+}
+
+const topRightContainer = {
+  float: "left"
+}
 class WelcomePage extends React.Component<{}> {
   render(){
     return (
-  <div>
-    <span> Velkommen til Røde Kors appen! </span>
-  </div>
+  <div className="full" style={divStyle}>
+    <div className="container-fluid" style={topContainer}>
+      <div className="container" style={topLeftContainer}>
+        <div className="container" style={topLeftInner1}>
+      <span> Velkommen til Røde Kors appen! </span>
+      </div>
+      <div className="container" style={topleftInner2}>
+        <span>Hvis vi får tid</span>
+      </div>
+      </div>
+      <div className="container" style={topRightContainer}>
+        <span>Her skal meldinger være</span>
+      </div>
+    </div>
+      <div className="container-fluid" style={kalenderShow}>
+        <div className="col-sm-12">
+            <div className="col-sm-2">
+              Kalenderdag
+            </div>
+            <div className="col-sm-2">
+              Kalenderdag
+            </div>
+            <div className="col-sm-2">
+              Kalenderdag
+            </div>
+            <div className="col-sm-2">
+              Kalenderdag
+            </div>
+            <div className="col-sm-2">
+              Kalenderdag
+            </div>
+            <div className="col-sm-2">
+              Kalenderdag
+            </div>
+          </div>
+        </div>
+      </div>
   )
   }
 }
