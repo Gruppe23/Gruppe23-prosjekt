9a17b02d0a368b283af8b727b4d6b1daf5938c6d
Tsikio <Tsikio@noreply.github.com>
2018-04-23 19:22:32 +0200

    Ferdig app

--- main.js
+++ main.js
@@ -7,8 +7,6 @@ let mainWindow;
 app.on('ready', () => {
   mainWindow = new BrowserWindow({width: 1200, frame: false, height: 900, minWidth: 1000, minHeight: 780, icon: path.join(__dirname, 'assets/icons/png/64x64.png') });
 
-  // Open Development Tools
-  mainWindow.openDevTools();
 
   mainWindow.loadURL('file://' + __dirname + '/index.html');
 });
